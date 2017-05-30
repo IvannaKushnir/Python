f2ad2d5d7d38c2b8580f51ceba1eb356ef52e658
IvannaKushnir <plokii.ua@gmail.com>
2017-05-30 14:39:11 +0300

    Add files via upload

--- file.py
+++ file.py
@@ -1,6 +1,13 @@
 import csv
 
-class Student:
+class Person:
+	def __init__(self, name, surname, age, gender):
+		self.name = name
+		self.surname = surname
+		self.age = age
+		self.gender = gender	
+
+class Student(Person):
 	def __init__(self, name, surname, course, age, gender):
 		self.name = name
 		self.surname = surname
@@ -8,17 +15,27 @@ class Student:
 		self.age = age
 		self.gender = gender
 
+
+	def __init__(self, row):
+		self.name = row[0].strip()
+		self.surname = row[1].strip()
+		self.course = row[2].strip()
+		self.age = int(row[3].strip())
+		self.gender = row[4].strip()		
+		students.append(student)
+
 students= []
 
 f = open('file_1.csv', 'r')
 reader = csv.reader(f)
 for row in reader:
-    student = Student(row[0].strip(), row[1].strip(), row[2].strip(), int(row[3].strip()), row[4].strip())
-	students.append(student)
 	print row
-	
 f.close()
 
 for student in students:
 	if student.age >30:
 		print student.name + " " + student.surname
+
+	def Search(name, surname):
+		for i in students:
+		    return "Hello, {} {}. Are you {} years old?".format(name, surname, age)
