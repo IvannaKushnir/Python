4d286b656b0c27b835da9172ce35bd77872a08b2
IvannaKushnir <plokii.ua@gmail.com>
2017-06-07 14:58:27 +0300

    Add files via upload

--- file.py
+++ file.py
@@ -8,22 +8,21 @@ class Person:
 		self.gender = gender	
 
 class Student(Person):
-	Person.__init__(self, name, surname, age, gender)
-	self.course = course
-
 	def __init__(self, row):
-		Student.__init__(self, row[0].strip(), row[1].strip(), row[2].strip(), int(row[3].strip()), row[4].strip())
-
-	def __str__(self):
-		return "Name: {}, Surname: {}, Age: {} years old, Gender: {}".format(self.name, self.surname, self.age, self.gender)
-		Students.append(s)
-
+		Person.__init__(self, row[0].strip(), row[1].strip(), int(row[3].strip()), row[4].strip())
+		self.course = row[2].strip()
 
 students= []
+
+	def __str__(self, row):
+		return "Name: {}, Surname: {}, Age: {} years old, Gender: {}".format(self.name, self.surname, self.age, self.gender)
+		students.append(s)
+
 
 f = open('file_1.csv', 'r')
 reader = csv.reader(f)
 for row in reader:
+	s=Student(row)
 	print row
 f.close()
 
