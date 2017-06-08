6b4135f5b86b4976cc1782a370ae498d5ad54006
IvannaKushnir <plokii.ua@gmail.com>
2017-06-08 16:09:44 +0300

    Add files via upload

--- file.py
+++ file.py
@@ -1,5 +1,4 @@
 import csv
-
 class Person:
 	def __init__(self, name, surname, age, gender):
 		self.name = name
@@ -12,25 +11,50 @@ class Student(Person):
 		Person.__init__(self, row[0].strip(), row[1].strip(), int(row[3].strip()), row[4].strip())
 		self.course = row[2].strip()
 
-students= []
+	def __str__(self):
+		return "Name: {}, Surname: {}, Age: {} years old, Gender: {}, Course: {}".format(self.name, self.surname, self.age, self.gender, self.course)
 
-	def __str__(self, row):
-		return "Name: {}, Surname: {}, Age: {} years old, Gender: {}".format(self.name, self.surname, self.age, self.gender)
+students = []
+try: 
+	f = open('file_1.csv', 'r')
+	reader = csv.reader(f)
+	for row in reader:
+		s = Student(row)
 		students.append(s)
+	f.close()
 
+except IOError:
+	print "File not found"
 
-f = open('file_1.csv', 'r')
-reader = csv.reader(f)
-for row in reader:
-	s=Student(row)
-	print row
-f.close()
+else:
+	for student in students:
+		if student.age > 30:
+			print student
 
-for student in students:
-	if student.age >30:
-		print student.name + " " + student.surname
-
+	print "1st search function"
 	def Search(name, surname):
 		for i in students:
-			if name == name and surname == surname:
+			if i.name == name and i.surname == surname:
 				return i
+
+	a = Search ("Harry", "Grey")
+	print a
+
+	print "2nd search function"
+	def SecondSearch(age):
+		for k in students:
+			if k.age == age:
+				yield k
+
+	a = SecondSearch (int(18))
+
+	for m in a:
+		print m
+
+	print "3rd search function"
+	print students[1]
+
+finally:
+	print "End"
+
+
